4a7034cb0efcdf61a86c6eab747f8a7e35a131ee
Vijay <vjhoelas@gmail.com>
2020-04-19 18:56:09 +0200

    repository cloned prt 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage|PHP-Opdrachten</title>
+</head>
+<body>
+<a href="H1/h1Opdr1.php">opdr 1 </a>
+</body>
+</html>
